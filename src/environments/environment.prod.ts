384867a0e42734ea68b329fe0300754ca1f5b8f5
RAUSHAN ANAND <43677453+raushan202000@users.noreply.github.com>
2020-02-08 02:33:29 +0530

    Second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyACiiKMhUvo8UZQ_NEemX36THURo1GK_q0",
+    authDomain: "database-ad12f.firebaseapp.com",
+    databaseURL: "https://database-ad12f.firebaseio.com",
+    projectId: "database-ad12f",
+    storageBucket: "database-ad12f.appspot.com",
+    messagingSenderId: "192458884902",
+    appId: "1:192458884902:web:45f1fb38e19d4ffe6e63e7",
+    measurementId: "G-67END6ZREJ"
+  },
 };
